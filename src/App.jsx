7af7abf15a50316306afc6bf6049ae2f78757412
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-08 23:32:21 +0600

    set Google with login btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import "./App.css";
+import { auth } from "./firebase/firebase.init";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, seruser] = useState(null);
+
+  const googleProvider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log(result.user);
+        seruser(result.user);
+      })
+      .catch((error) => console.log("ERROR", error));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Simple Dimple Trimple Firebase</h1>
+      <button onClick={handleGoogleSignIn}>Login with Google</button>
+      {user && (
+        <div>
+          <h3>Name: {user.displayName}</h3>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" />
+          <p>Last Login: {user.metadata.lastSignInTime}</p>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/__Steps__.js
+++ src/__Steps__.js
@@ -5,5 +5,5 @@
  *  4. npm install firebase
  *  5. get firebase config and [you  should not] and the config in your project [do not save or share your config]
  *  6. enable sign in method that  you want to use: Build > Authentication > Get Started > Click on Google > Enable > Set the email address > Save
- *  7. go to firebase docs
+ *  7. go to firebase docs  
  */
