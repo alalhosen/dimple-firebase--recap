9afb6215134fe1af1ca48f258fc4de46e690e1ea
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-09 20:53:03 +0600

    some sentense add in __Steps__.js file

--- src/__Steps__.js
+++ src/__Steps__.js
@@ -6,4 +6,9 @@
  *  5. get firebase config and [you  should not] and the config in your project [do not save or share your config]
  *  6. enable sign in method that  you want to use: Build > Authentication > Get Started > Click on Google > Enable > Set the email address > Save
  *  7. go to firebase docs  
+ *  8. create auth (firebase initialize auth instance)
+ *  9 go to the login button. add event handler and create a provider (google provider)
+ *  10. call the signInWithProvider (auth, googleProvider)
+ *  11. after successfully getting data, you may set it to a state
+ * 
  */
